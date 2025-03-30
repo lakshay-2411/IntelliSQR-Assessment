1fed09cbeeb7972fe47fd7b3cf5b7cba794a3ed7
lakshay-2411 <lakshaynandwani06@gmail.com>
2025-03-30 22:09:43 +0530

    Made user route

---
+++ backend/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import userRoutes from "./routes/userRoutes";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use("/api", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
